01ec7ac3bd2d43ccccf6867daee14232a6fb0aca
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-26 14:44:27 +0530

    Added mrmr feature selection

--- pd-prediction-knn-mrmr.py
+++ pd-prediction-knn-mrmr.py
@@ -1,17 +1,21 @@
-
 # %%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from pymrmre import mrmr
-
+from sklearn.model_selection import train_test_split
+import mifs
 # %%
 dataset = pd.read_csv('data/parkinsons.csv')
-X = dataset.iloc[:, 1:-1]   # input feactures 
+X = dataset.iloc[:, 1:-1]   # input feactures
 y = dataset.iloc[:, -1]     # output
-
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=0)
 # %%
-solution = mrmr.mrmr_ensemble(features=X,targets=y,solution_length=5,solution_count=3)
-
-print(solution)
-# https://aka.ms/vs/16/release/vc_redist.x64.exe
+for i in range(1, 22):
+    feat_selector = mifs.MutualInformationFeatureSelector('MRMR', k=i)
+    feat_selector.fit(X_train, y_train)
+    # call transform() on X to filter it down to selected features
+    X_filtered = feat_selector.transform(X_train.values)
+    # Create list of features
+    feature_name = X_train.columns[feat_selector.ranking_]
+    print(feature_name)
