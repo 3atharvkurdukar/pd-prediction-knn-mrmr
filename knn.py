bcf9f1f6e500bfa4bbbc974ddc682c09435c39b5
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2021-05-26 14:46:32 +0530

    Improved knn layout

--- knn.py
+++ knn.py
@@ -4,23 +4,19 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score 
-from fknn import FuzzyKNN
+from sklearn.metrics import accuracy_score
+# from fknn import FuzzyKNN
 import sklearn.feature_selection as fs
-
-#This is file for Knn distance function from here we can import 3 distance function to our main program file
-
-"""fit_transform() is used on the training data so that we can scale the training data and also learn the scaling parameters of that data.
- Here, the model built by us will learn the mean and variance of the features of the training set. These learned parameters are then used to 
- scale our test data."""
 
 # %%
 dataset = pd.read_csv('data/parkinsons.csv')
-X = dataset.iloc[:, 1:-1]   # input feactures 
+X = dataset.iloc[:, 1:-1]   # input feactures
 y = dataset.iloc[:, -1]     # output
 
 # %%
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=0)
+
 
 # %%
 def euclidean_dist(X_train, X_test, y_train, y_test):
@@ -29,11 +25,13 @@ def euclidean_dist(X_train, X_test, y_train, y_test):
     X_test = sc.transform(X_test)
     result = []
     for i in range(4, 11):
-        classifier = KNeighborsClassifier(n_neighbors = i,metric= 'euclidean' ,p=2)
+        classifier = KNeighborsClassifier(
+            n_neighbors=i, metric='euclidean', p=2)
         classifier.fit(X_train, y_train)
         y_pred = classifier.predict(X_test)
         result.append(accuracy_score(y_test, y_pred))
     return max(result), result.index(max(result)) + 4
+
 
 # %%
 def mahalanobis_dist(X_train, X_test, y_train, y_test):
@@ -42,11 +40,13 @@ def mahalanobis_dist(X_train, X_test, y_train, y_test):
     X_test = sc.transform(X_test)
     result = []
     for i in range(4, 11):
-        classifier = KNeighborsClassifier(metric='mahalanobis', n_neighbors = i, metric_params={'VI': np.cov(X)})
+        classifier = KNeighborsClassifier(
+            metric='mahalanobis', n_neighbors=i, metric_params={'VI': np.cov(X)})
         classifier.fit(X_train, y_train)
         y_pred = classifier.predict(X_test)
         result.append(accuracy_score(y_test, y_pred))
     return max(result), result.index(max(result)) + 4
+
 
 # %%
 def correlation_dist(X_train, X_test, y_train, y_test):
@@ -55,18 +55,19 @@ def correlation_dist(X_train, X_test, y_train, y_test):
     X_test = sc.transform(X_test)
     result = []
     for i in range(4, 11):
-        classifier = KNeighborsClassifier(n_neighbors = i,metric= 'correlation')
+        classifier = KNeighborsClassifier(n_neighbors=i, metric='correlation')
         classifier.fit(X_train, y_train)
         y_pred = classifier.predict(X_test)
         result.append(accuracy_score(y_test, y_pred))
     return max(result), result.index(max(result)) + 4
+
 
 # %%
 def fuzzy(X_train, X_test, y_train, y_test):
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
     X_test = sc.transform(X_test)
-    result =[]
+    result = []
     for i in range(5, 12, 2):
         classifier = FuzzyKNN(k=i)
         classifier.fit(X_train, y_train)
@@ -74,16 +75,20 @@ def fuzzy(X_train, X_test, y_train, y_test):
         result.append(accuracy_score(y_test, y_pred))
     return max(result), result.index(max(result)) + 4
 
+
+# %%
 def weighted_knn(X_train, X_test, y_train, y_test):
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
     X_test = sc.transform(X_test)
     result = []
     for i in range(4, 11):
-        mutual_info = fs.mutual_info_classif(X_train, y_train, discrete_features='auto', n_neighbors=i, copy=True, random_state=None)
-        X_train_new = X_train * mutual_info 
+        mutual_info = fs.mutual_info_classif(
+            X_train, y_train, discrete_features='auto', n_neighbors=i, copy=True, random_state=None)
+        X_train_new = X_train * mutual_info
         result.append(euclidean_dist(X_train_new, X_test, y_train, y_test))
     return max(result), result.index(max(result)) + 4
+
 
 # %%
 print(euclidean_dist(X_train, X_test, y_train, y_test))
@@ -94,5 +99,3 @@ print(correlation_dist(X_train, X_test, y_train, y_test))
 # %%
 # print(fuzzy(X_train, X_test, y_train, y_test))
 print(weighted_knn(X_train, X_test, y_train, y_test))
-
-# https://arnabfly.github.io/arnab_blog/fknn/#:~:text=Let%27s%20understand%20fuzzy%20k%2DNN,have%20data%20sets%20with%20classes.&text=There%20are%20three%20classes%20and,and%20P%20using%20Euclidean%20Distance.
